febf4df4315ba30434dcd1823200b3a944c80c97
Zaier <veraalber989@gmail.com>
2021-12-08 14:34:37 -0300

    Debuggin en un script con las opciones -v y -x

---
+++ 06_Debug.sh
@@ -0,0 +1,11 @@
+# ! /bin/bash
+# Autor: Zaier / veraalber989@gmail.com
+
+# Debug en Script
+
+# -v -> Utilizado para ver el resultado detallado de nuestro script, evaluado linea por linea.
+
+# -x -> Utilizado para desplegar la informacion de los comandos que son usados, capturando el comando y su salida.
+
+# Ejemplo: $ bash -v 01_utilityPostgres.sh
+# Ejemplo: $ bash -x 01_utilityPostgres.sh

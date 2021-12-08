f02ba18a9bbea79e85a1edd95ddef3a8aa75de58
Zaier <veraalber989@gmail.com>
2021-12-08 12:51:47 -0300

    Script que espera dos argumentos para ser ejecutado

--- 4_argumentos.sh
+++ 4_argumentos.sh
@@ -1,4 +1,10 @@
 # ! /bin/bash
-# Programa para revisar los tipos de operadores
+# Programa para ejemplificar el paso de argumentos. En este caso para ejecutar el el programa hay que pasarle dos parametros.
 # Autor: Zaier / veraalber989@gmail.com
 
+nombreCurso=$1
+horarioCurso=$2
+
+echo "El nombre del curso es: $nombreCurso, dictado en el horario de: $horarioCurso"
+echo "El numero de parametros enviados es: $#"
+echo "Los parametros enviados son: $*"

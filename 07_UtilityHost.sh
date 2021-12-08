bf54cd47d0c2b88b0e43f7818871d04c0ae3d33a
Zaier <veraalber989@gmail.com>
2021-12-08 14:51:58 -0300

    Reto de un programa que interactua con el cliente

---
+++ 07_UtilityHost.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+# Programa interactivo. Reto 1
+# Autor: Zaier / veraalber989@gmail.com
+
+echo "Cual es tu nombre?: "
+read nombre
+echo "Que edad tienes?: "
+read edad
+echo "Tu eres $nombre y tienes $edad años"

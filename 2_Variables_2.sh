0aefc326068b96023ca7d9324caac13dfa5107e2
Zaier <veraalber989@gmail.com>
2021-12-08 12:39:57 -0300

    Variables, Tipos de datos y argumentos

---
+++ 2_Variables_2.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+# Programa para revisar la declaracion de variables.
+# Autor: Zaier / veraalber989@gmail.com
+
+echo "Opcion nombre del script anterior: $nombre"
+
---
+++ 4_argumentos.sh
@@ -0,0 +1,4 @@
+# ! /bin/bash
+# Programa para revisar los tipos de operadores
+# Autor: Zaier / veraalber989@gmail.com
+

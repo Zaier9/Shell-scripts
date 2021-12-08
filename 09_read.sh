2e3ff74a66bf2072296eee3fb2b2f7bb4df3c6c6
Zaier <veraalber989@gmail.com>
2021-12-08 15:45:34 -0300

    Script para capturar informacion del usuario

---
+++ 09_read.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# Programa para ejemplificar como capturar la informacion del usuario utilizando el comando read 
+# Autor: Zaier / veraalber989@gmail.com
+
+option=0
+backupName=""
+
+echo "Programa Utilidades Postgres"
+read -p "Ingresar una opcion: " option
+read -p "Ingresar el nombre del archivo del backup:" backupName 
+echo "Opcion:$option, backupName:$backupName"
+
